692beef822284267b8488c7b4b3b36333d42ebef
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2024-10-25 21:31:31 -0300

    new: Importação das classes de Action no __init__.py

--- src/pyroles/models/__init__.py
+++ src/pyroles/models/__init__.py
@@ -1,3 +1,5 @@
+from .actions import Action as Action
+from .actions import UuidAction as UuidAction
 from .roles import Role as Role
 from .roles import UuidRole as UuidRole
 from .roles import RoleHelper as RoleHelper
