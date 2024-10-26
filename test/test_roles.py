aa0cc6da51702f1dc34d6d2795ba34a33e2eb3f5
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2024-10-25 21:28:22 -0300

    new: Setup dos testes de roles

--- test/test_roles.py
+++ test/test_roles.py
@@ -19,3 +19,29 @@ class User(Model):
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     roles = relationship("Role", secondary="user_role", back_populates="users")
 
+class TestRoles(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.engine = create_engine('sqlite:///:memory:')
+        cls.Session = sessionmaker(bind=cls.engine)
+        Base.metadata.create_all(cls.engine)
+        cls.faker: Faker = Faker()
+    
+    def setUp(self):
+        self.session = self.Session()
+        user: User = User(name=self.faker.name())
+        self.session.add(user)
+        self.session.commit()
+        self.session.refresh(user)
+        self.user = user
+        self.roleHelper = RoleHelper(self.session, Role)
+        
+    def tearDown(self):
+        self.session.close()
+        self.roleHelper.session.close()
+        
+
+        
+        
+if __name__ == '__main__':
+    main()
