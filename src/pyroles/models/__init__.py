b407a09d17aa5b000278041939096203ff47f4b5
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2024-10-25 21:25:34 -0300

    new: importação das classes em roles.py

---
+++ src/pyroles/models/__init__.py
@@ -0,0 +1,5 @@
+from .roles import Role as Role
+from .roles import UuidRole as UuidRole
+from .roles import RoleHelper as RoleHelper
+from .roles import UserRole as UserRole
+from .roles import UuidUserRole as UuidUserRole
